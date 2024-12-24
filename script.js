a7eaf7141a853afee66ce65e2664817ea893359c
aaryanrsh <aaryanrajshrestha1@gmail.com>
2024-12-23 21:57:37 -0500

    added hero image w styling changes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navContainer = document.querySelector('.nav-container');
+
+    menuToggle.addEventListener('click', function() {
+        navContainer.classList.toggle('active');
+    });
+});
